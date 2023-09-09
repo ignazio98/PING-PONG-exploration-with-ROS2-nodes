4fa9369316c9504f6033102d194604e3a2d72a1a
ignazio98 <damicisignazio.gmail.com>
2023-09-09 11:20:22 +0200

    Launch File upload

--- src/py_echo/launch/launch-all.py
+++ src/py_echo/launch/launch-all.py
@@ -10,138 +10,138 @@ def generate_launch_description():
 	
 	file1 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch1_4.py'))
+			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch1-4.py'))
 	)
 	file2 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch1_16.py'))
+			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch1-16.py'))
 	)
 	file3 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch1_64.py'))
+			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch1-64.py'))
 	)
 	file4 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch1_256.py'))
+			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch1-256.py'))
 	)
 	file5 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch1_1024.py'))
+			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch1-1024.py'))
 	)
 	file6 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch1_4096.py'))
+			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch1-4096.py'))
 	)
 	file7 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch1_16384.py'))
+			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch1-16384.py'))
 	)
 	
 	file8 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch2_4.py'))
+			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch2-4.py'))
 	)
 	file9 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch2_16.py'))
+			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch2-16.py'))
 	)
 	file10 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch2_64.py'))
+			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch2-64.py'))
 	)
 	file11 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch2_256.py'))
+			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch2-256.py'))
 	)
 	file12 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch2_1024.py'))
+			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch2-1024.py'))
 	)
 	file13 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch2_4096.py'))
+			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch2-4096.py'))
 	)
 	file14 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch2_16384.py'))
+			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch2-16384.py'))
 	)
 	
 	file15 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch4_4.py'))
+			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch4-4.py'))
 	)
 	file16 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch4_16.py'))
+			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch4-16.py'))
 	)
 	file17 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch4_64.py'))
+			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch4-64.py'))
 	)
 	file18 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch4_256.py'))
+			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch4-256.py'))
 	)
 	file19 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch4_1024.py'))
+			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch4-1024.py'))
 	)
 	file20 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch4_4096.py'))
+			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch4-4096.py'))
 	)
 	file21 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch4_16384.py'))
+			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch4-16384.py'))
 	)
 	
 	file22 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch8_4.py'))
+			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch8-4.py'))
 	)
 	file23 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch8_16.py'))
+			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch8-16.py'))
 	)
 	file24 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch8_64.py'))
+			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch8-64.py'))
 	)
 	file25 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch8_256.py'))
+			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch8-256.py'))
 	)
 	file26 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch8_1024.py'))
+			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch8-1024.py'))
 	)
 	file27 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch8_4096.py'))
+			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch8-4096.py'))
 	)
 	file28 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch8_16384.py'))
+			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch8-16384.py'))
 	)
 	
 	file29 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch1_65356.py'))
+			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch1-65356.py'))
 	)
 	
 	file31 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch2_65356.py'))
+			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch2-65356.py'))
 	)
 	
 	file33 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch4_65356.py'))
+			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch4-65356.py'))
 	)
 	
 	file35 = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource(
-			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch8_65356.py'))
+			os.path.join(get_package_share_directory('cpp_echo'), 'launch/launch8-65356.py'))
 	)
 	
 	ld.add_action(file1)
