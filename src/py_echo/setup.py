3beb6ecec059ad81ae9a4f591cb6146dd6421dfe
ignazio98 <damicisignazio.gmail.com>
2023-10-02 20:03:33 +0200

    cmakefile

--- src/py_echo/setup.py
+++ src/py_echo/setup.py
@@ -23,8 +23,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
             'console_scripts': [
-                    'talker = py_echo.publisher_member_function:main',
-                    'listener = py_echo.subscriber_member_function:main',
+                    'client = py_echo.publisher_member_function:main',
+                    'server = py_echo.subscriber_member_function:main',
             ],
     },
     
